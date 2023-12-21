11b0f6e027e3853b6014ec18a621f2bc65af1ffb
Anya Dya <143488682+anyadya09@users.noreply.github.com>
2023-12-21 13:59:29 +0800

    up

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 document.addEventListener('alpine:init', () => {
-
     Alpine.data('products', () => ({
  
         items: [
             { id: 1, name: 'chellapa1', img: '1.jpg', price: 2000 },
-            { id: 1, name: 'chellapa2', img: '1.jpg', price: 3000 },
-            { id: 1, name: 'chellapa3', img: '1.jpg', price: 4000 },
-            { id: 1, name: 'chellapa4', img: '1.jpg', price: 5000 },
+            { id: 2, name: 'chellapa2', img: '2.jpg', price: 3000 },
+            { id: 3, name: 'chellapa3', img: '3.jpg', price: 4000 },
+            { id: 4, name: 'chellapa4', img: '4.jpg', price: 5000 },
         ],
-        }))
-})
+        }));
+});
